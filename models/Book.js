b11eaca3505c0c8bce5c289bf0db38263f6315ae
m0hx777 <m0hx@protonmail.com>
2024-01-21 12:09:10 +0300

    fixed book controller and model. api works!

--- models/Book.js
+++ models/Book.js
@@ -2,37 +2,37 @@ const mongoose = require('mongoose');
 
 // Book Schema
 const bookSchema = mongoose.Schema({
-"title" : {
-    type: String,
-    required: true
-},
-"author" : 
-{   type: String,
-    required: true
-},
-"description" : String,
-"isbn" : String,
-"publish_date" : Date,
-"image" : String,
+    "title": {
+        type: String,
+        required: true
+    },
+    "author": {
+        type: String,
+        required: true
+    },
+    "description": String,
+    "isbn": String,
+    "publish_date": Date,
+    "image": [String], // Array of strings for images
 
-"category":{
-    type: mongoose.Schema.Types.ObjectId,
-    ref:'Category'
-},
-
-"library":{
-    type: mongoose.Schema.Types.ObjectId,
-    ref:'Library'
-},
-"review":{
-    type: mongoose.Schema.Types.ObjectId,
-    ref:'Review'
-},
-
-},
-{
-  timestamps: true
-})
+    "category": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: false // Make it optional
+    },
+    "library": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Library',
+        required: false // Make it optional
+    },
+    "review": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Review',
+        required: false // Make it optional
+    },
+}, {
+    timestamps: true
+});
 
 const Book = mongoose.model("Book", bookSchema);
 
