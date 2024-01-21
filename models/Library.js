2034942da6526727cbfc424837b0bc0e4c5549ac
faisal86 <faisalkarim203@gmail.com>
2024-01-21 14:54:15 +0300

    changes-model

--- models/Library.js
+++ models/Library.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 // Category Schema
 const librarySchema = mongoose.Schema({
-    "book":{
+    "book":[{
         type: mongoose.Schema.Types.ObjectId,
         ref:'Book',
 
-    },
+    }],
     "user":{
         type: mongoose.Schema.Types.ObjectId,
         ref:'User',
