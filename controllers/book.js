1bf11be6dfe2f5fc8c72a793e6f0f10ec24734b1
m0hx777 <m0hx@protonmail.com>
2024-01-22 11:51:52 +0300

    awo!

--- controllers/book.js
+++ controllers/book.js
@@ -2,6 +2,8 @@ const Book = require('../models/Book');
 const Category = require('../models/Category');
 const fs = require("fs");
 const uploadCloudinary = require('../helper/cloudUploader');
+const { uploadMultiple } = require('../helper/cloudUploader'); // Adjust the path as necessary
+
 
 exports.book_create_post = async (req, res) => {
     try {
@@ -55,68 +57,156 @@ exports.get_mybook_get = (req, res) => {
         });
 };
 
+// exports.book_edit_post = async (req, res) => {
+//     console.log(req.body)
+//     if(req.files && req.files.length != 0){
+//         let images;
+//         let pathDb = [];
+//         images = req.files.map(file => `public/images/${file.filename}`);
+//         await uploadCloudinary.upload_multiple(images)
+//         .then((imagesPath) =>{
+//             imagesPath.forEach(pathImg =>{
+//                 console.log(pathImg.url)
+//                 pathDb.push(pathImg.url);
+//             })
+//             images.forEach(remove =>{
+//                 // To remove the image from public/images and store it in cloudinary only
+//                 fs.unlink(remove, (err) => {
+//                     if (err) {
+//                         console.error(err);
+//                     } else {
+//                         console.log('File is deleted.');
+//                     }
+//                     });    
+//             })
+//             Category.findById(req.body.category)
+//             .then((category) => {
+//                 category.book.push(book);
+//                 category.save();
+//             })
+//             .catch((err) => {
+//                 console.log(err);
+//             });
+//             const body = req.body;
+//             // console.log(pathDb);
+//             body.image = pathDb;
+//             console.log(body.image)
+//             Book.findByIdAndUpdate(req.body._id, body, {new: true})
+//             .then((newBook) => {
+//                 console.log(newBook)
+//                 res.json(newBook);
+//             })
+//             .catch((err) => {
+//                 console.log(err);
+//                 res.status(500).send('Internal Server Error');
+//             });
+//         })
+//         .catch((err) =>{
+//             console.log(err);
+//         })    
+    
+//     }
+//     else{
+//         console.log('not image')
+//         Book.findByIdAndUpdate(req.body._id, req.body, {new: true})
+//         .then((newBook) => {
+//             console.log(newBook)
+//             res.json(newBook);
+//         })
+//         .catch((err)=>{
+//             console.log(err)
+//         })
+//         } 
+    
+// }
+
+
+// exports.book_edit_post = async (req, res) => {
+//     const bookId = req.params.book; // Get the book ID from URL parameter
+
+//     try {
+//         // Fetch the current book data from the database
+//         const currentBook = await Book.findById(bookId);
+//         if (!currentBook) {
+//             return res.status(404).send('Book not found');
+//         }
+
+//         let updateData = {};
+
+//         // Compare and update only the changed fields
+//         Object.keys(req.body).forEach(key => {
+//             if (req.body[key] !== currentBook[key]) {
+//                 updateData[key] = req.body[key];
+//             }
+//         });
+
+//         // Handle file upload if any files are provided
+//         if (req.files && req.files.length != 0) {
+//             let images = req.files.map(file => `public/images/${file.filename}`);
+//             let pathDb = [];
+//             const imagesPath = await uploadCloudinary.upload_multiple(images);
+//             imagesPath.forEach(pathImg => {
+//                 pathDb.push(pathImg.url);
+//             });
+//             updateData.image = pathDb; // Update images
+//         }
+
+//         // If there are fields to update, proceed with the update
+//         if (Object.keys(updateData).length > 0) {
+//             const updatedBook = await Book.findByIdAndUpdate(bookId, updateData, { new: true });
+//             res.json(updatedBook);
+//         } else {
+//             res.json({ message: 'No fields were updated', book: currentBook });
+//         }
+
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Internal Server Error');
+//     }
+// };
+
+
 exports.book_edit_post = async (req, res) => {
-    console.log(req.body)
-    if(req.files && req.files.length != 0){
-        let images;
-        let pathDb = [];
-        images = req.files.map(file => `public/images/${file.filename}`);
-        await uploadCloudinary.upload_multiple(images)
-        .then((imagesPath) =>{
-            imagesPath.forEach(pathImg =>{
-                console.log(pathImg.url)
-                pathDb.push(pathImg.url);
-            })
-            images.forEach(remove =>{
-                // To remove the image from public/images and store it in cloudinary only
-                fs.unlink(remove, (err) => {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        console.log('File is deleted.');
-                    }
-                    });    
-            })
-            Category.findById(req.body.category)
-            .then((category) => {
-                category.book.push(book);
-                category.save();
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-            const body = req.body;
-            // console.log(pathDb);
-            body.image = pathDb;
-            console.log(body.image)
-            Book.findByIdAndUpdate(req.body._id, body, {new: true})
-            .then((newBook) => {
-                console.log(newBook)
-                res.json(newBook);
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(500).send('Internal Server Error');
-            });
-        })
-        .catch((err) =>{
-            console.log(err);
-        })    
-    
+    const bookId = req.params.book;
+
+    try {
+        const currentBook = await Book.findById(bookId);
+        if (!currentBook) {
+            return res.status(404).send('Book not found');
+        }
+
+        let updateData = {};
+        Object.keys(req.body).forEach(key => {
+            if (req.body[key] !== currentBook[key]) {
+                updateData[key] = req.body[key];
+            }
+        });
+
+        if (req.files && req.files.length != 0) {
+            let images = req.files.map(file => `./public/uploads/${file.filename}`);
+            try {
+                const imagesPath = await uploadMultiple(images);
+                updateData.image = imagesPath;
+            } catch (err) {
+                console.error(err);
+                return res.status(500).send('Error uploading images');
+            }
+        }
+        
+
+        if (Object.keys(updateData).length > 0) {
+            const updatedBook = await Book.findByIdAndUpdate(bookId, updateData, { new: true });
+            res.json(updatedBook);
+        } else {
+            res.json({ message: 'No fields were updated', book: currentBook });
+        }
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
     }
-    else{
-        console.log('not image')
-        Book.findByIdAndUpdate(req.body._id, req.body, {new: true})
-        .then((newBook) => {
-            console.log(newBook)
-            res.json(newBook);
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-        } 
-    
-}
+};
+
 
 exports.book_delete_get = (req, res) => {
     Book.findByIdAndDelete(req.query.id)
