b91a0c5061913b5a0a58a69f0ccc969fde6f0bb7
m0hx777 <m0hx@protonmail.com>
2024-01-24 20:26:04 +0300

    backend reader fix ,-,

--- helper/multerUploader.js
+++ helper/multerUploader.js
@@ -1,11 +1,19 @@
+// In your Multer configuration file
 const multer = require('multer');
+const path = require('path');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/uploads');
+        if (file.mimetype.startsWith('image')) {
+            cb(null, './public/uploads'); // For images
+        } else if (file.mimetype === 'application/epub+zip') {
+            cb(null, './public/uploads/epubs'); // For EPUB files
+        } else {
+            cb(new Error('Invalid file type'), false); // Reject other file types
+        }
     },
     filename: function (req, file, cb) {
-        let filename = file.fieldname + '-' + Date.now() + '-' + file.originalname;
+        let filename = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
         cb(null, filename);
     }
 });
--- routes/book.js
+++ routes/book.js
@@ -3,15 +3,24 @@ const router = express.Router();
 const bookController = require('../controllers/book');
 const upload = require('../helper/multerUploader');
 
+
 router.use(express.json());
 
 
-router.post('/create', upload.array('file'), bookController.book_create_post);
+// router.post('/create', upload.array('file'), bookController.book_create_post);
+// router.post('/update/:book', upload.array('file'), bookController.book_edit_post);
+
+
+
 router.get('/index', bookController.book_index_get);
 // router.get('/mybooks', bookController.get_mybook_get);
-router.post('/update/:book', upload.array('file'), bookController.book_edit_post);
+
 router.post('/delete/:id', bookController.book_delete_get);
 router.get('/detail', bookController.book_detail_get);
+
+router.post('/create', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'epubFile', maxCount: 1 }]), bookController.book_create_post);
+router.post('/update/:book', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'epubFile', maxCount: 1 }]), bookController.book_edit_post);
+
 // GET /byCategory
 router.get('/byCategory', bookController.book_getByCategory_get);
 module.exports = router;
